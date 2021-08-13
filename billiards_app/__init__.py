81e76dee710405ec52ba7207ea72354e47c060bc
Bryan <bryan@poolshark.experiment.com>
2021-08-13 17:54:29 -0400

    update init from remote server

--- billiards_app/__init__.py
+++ billiards_app/__init__.py
@@ -5,10 +5,10 @@ import json
 from flask_cors import CORS
 
 app = Flask(__name__)
-#with open('/etc/config.json') as config_file:
-#    config = json.load(config_file)
-app.config['SECRET_KEY'] = "fljhgsdfljkasdhgf"#config.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///localData.db" #config.get('SQLALCHEMY_DATABASE_URI')
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+app.config['SECRET_KEY'] = config.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 CORS(app)
