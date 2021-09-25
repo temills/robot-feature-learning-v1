9fa5485594bc9668db11867353f7edae4d0dd37e
Bryan <bsg262@gmail.com>
2021-09-25 16:33:22 -0400

    created new version of the experiment with 2 separate blocks for causal+animacy & counterfactual ratings. beta version has 3 trials each.

--- billiards_app/views.py
+++ billiards_app/views.py
@@ -17,12 +17,12 @@ def experiment():
         if d['trial_type'] == 'survey-text':
             session['prolificID'] = d['response']['prolificID']
             session['exp_trial'] = 0
-        elif 'In this experiment' in d['stimulus']:
+        #elif 'In this experiment' in d['stimulus']:
             print('found a new subject:' + d['subjectID'])
             subj = Subject(jspsychID=d['subjectID'], prolificID=session.get('prolificID'), date=datetime.datetime.now())
             db.session.add(subj)
             db.session.commit()
-        else: #d['trial_type'] == 'video-slider-response':
+        elif d['trial_type'] == 'video-slider-response':
             print('new trial data received')
             t = session.get('exp_trial')+1
             session['exp_trial'] = t
@@ -30,13 +30,12 @@ def experiment():
             trial_dat = Trial(trial_num=t,
                               jspsychID=d['subjectID'],
                               stimulus=d['stimulus'][0].split('/')[2],
+                              block=d['trial_type'],
                               time_elapse=d['time_elapsed'],
                               cause_resp=d['response_1'],
-                              cf_resp=d['response_2'],
-                              agent_resp=d['response_3'],
-                              rt_1=d['rt_1'],
-                              rt_2=d['rt_2'],
-                              rt_3=d['rt_3'],
+                              anim_resp=d['response_2'],
+                              cause_rt=d['rt_1'],
+                              anim_rt=d['rt_2'],
                               trial_rt=d['rt'],
                               subject_id=subj.id)
             if t == n_trls:
@@ -45,6 +44,31 @@ def experiment():
             db.session.add(trial_dat)
             db.session.commit()
 
+        elif d['trial_type'] == 'video-slider-response_CF':
+            print('new trial data received')
+            t = session.get('exp_trial')+1
+            session['exp_trial'] = t
+            subj = Subject.query.filter_by(jspsychID=d['subjectID']).first()
+            #trial_dat = Trial.query.filter_by(jspsychID=d['subjectID'], stimulus=d['stimulus'][0].split('/')[2])
+            #if trial_dat :
+            #    trial_dat.cf_resp = d['cf_response']
+            #    trial_dat.cf_rt = d['cf_rt']
+
+
+            trial_dat = Trial(trial_num=t,
+                              jspsychID=d['subjectID'],
+                              stimulus=d['stimulus'][0].split('/')[2],
+                              block=d['trial_type'],
+                              time_elapse=d['time_elapsed'],
+                              cf_resp=d['cf_response'],
+                              cf_rt=d['cf_rt'],
+                              trial_rt=d['rt'],
+                              subject_id=subj.id)
+            if t == n_trls:
+                subj.completion = True
+                db.session.add(subj)
+            db.session.add(trial_dat)
+            db.session.commit()
 
         return make_response("", 200)
 
